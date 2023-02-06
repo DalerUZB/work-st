56898eeb77910286c5f415102ae3e75c7c915e6c
macbook <macbook@MacBook-Pro-macbook.local>
2023-02-06 15:42:14 +0500

    finished

---
+++ src/pages/Home.jsx
@@ -0,0 +1,243 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import ImageOne from "../assets/Icons.svg";
+import ImageSecond from "../assets/Icons1.svg";
+import ImageThree from "../assets/Icons2.svg";
+import four from "../assets/Icons3.svg";
+const Home = () => {
+  const arr = [
+    {
+      icon: ImageOne,
+      name: "Type company name here",
+      id: 1,
+    },
+    {
+      icon: ImageSecond,
+      name: "Upload brief",
+      id: 2,
+    },
+    {
+      icon: four,
+      name: "Write your name",
+      id: 3,
+    },
+    {
+      icon: ImageThree,
+      name: "Your phone number",
+      id: 4,
+    },
+  ];
+  const [items, setItems] = useState(0);
+  function setTheme(index) {
+    setItems(index);
+  }
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="text">
+          Вы можете связаться с нами в любое время через
+        </div>
+        <div className="inputs">
+          {arr.map((item, index) => (
+            <div onClick={() => setTheme(index)} key={item.id}>
+              <span className="icon">
+                <img src={item.icon} alt={item.icon} />
+              </span>
+              <button className={items === index ? "theme" : ""}>
+                {item.name}
+              </button>
+            </div>
+          ))}
+          <button className="btn">Send your info now</button>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Home;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 99.2%;
+  min-height: 88.2vh;
+  max-height: auto;
+  background: #faf9fb;
+  padding-top: 100px;
+  margin-top: 4px;
+  .container {
+    display: flex;
+    flex-direction: column;
+    padding-left: 50px;
+    gap: 24px;
+    .text {
+      min-width: auto;
+      max-width: 843px;
+      height: 124px;
+      border-radius: nullpx;
+      font-family: "Outfit";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 46px;
+      line-height: 62px;
+      color: #000000;
+    }
+    .inputs {
+      min-width: auto;
+      max-width: 1000px;
+      max-height: auto;
+      min-height: 260px;
+      background: #f3f2f4;
+      border-radius: 18px;
+      display: flex;
+      flex-wrap: wrap;
+      align-content: flex-start;
+      padding: 30px 30px 0;
+      gap: 20px;
+      div:nth-child(1) {
+        width: 70%;
+        height: 50px;
+        font-family: "Outfit";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 30px;
+        display: flex;
+        position: relative;
+        button {
+          width: 100%;
+          height: 100%;
+          border-radius: 100px;
+          border: none;
+          text-align: left;
+          padding-left: 70px;
+          font-family: "Outfit";
+          font-style: normal;
+          font-weight: 400;
+          font-size: 24px;
+          line-height: 30px;
+          color: #fdfdfd;
+        }
+        .icon {
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          left: 12px;
+          background: rgba(255, 255, 255, 0.1);
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+          text-align: center;
+          padding: 6px;
+          img {
+            width: 100%;
+          }
+        }
+      }
+      div:nth-child(2) {
+        width: 27%;
+        height: 50px;
+        border-radius: 100px;
+        position: relative;
+        button {
+          width: 100%;
+          height: 100%;
+          border-radius: 100px;
+          border: none;
+          text-align: left;
+          padding-left: 60px;
+          border: 1px dashed #000000;
+        }
+        .icon {
+          position: absolute;
+          top: 50%;
+          left: 10px;
+          transform: translateY(-50%);
+          background: rgba(18, 18, 18, 0.1);
+          border-radius: 50%;
+          padding: 6px;
+          width: 30px;
+          height: 30px;
+          img {
+            width: 100%;
+          }
+        }
+      }
+      div:nth-child(3) {
+        width: 48.5%;
+        height: 50px;
+        border: 1px dashed #000000;
+        border-radius: 100px;
+        position: relative;
+
+        button {
+          width: 100%;
+          height: 100%;
+          border-radius: 100px;
+          border: none;
+          text-align: left;
+          padding-left: 60px;
+        }
+        .icon {
+          position: absolute;
+          top: 50%;
+          left: 10px;
+          transform: translateY(-50%);
+          background: rgba(18, 18, 18, 0.1);
+          border-radius: 50%;
+          padding: 6px;
+          width: 30px;
+          height: 30px;
+          img {
+            width: 100%;
+          }
+        }
+      }
+      div:nth-child(4) {
+        width: 48.5%;
+        height: 50px;
+        border: 1px dashed #000000;
+        border-radius: 100px;
+        position: relative;
+
+        button {
+          width: 100%;
+          height: 100%;
+          border-radius: 100px;
+          border: none;
+          text-align: left;
+          padding-left: 60px;
+        }
+        .icon {
+          position: absolute;
+          top: 50%;
+          left: 10px;
+          transform: translateY(-50%);
+          background: rgba(18, 18, 18, 0.1);
+          border-radius: 50%;
+          padding: 6px;
+          width: 30px;
+          height: 30px;
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+    .btn {
+      margin: 0 auto;
+      height: 50px;
+      min-width: 50%;
+      border-radius: 100px;
+      font-family: "Outfit";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 30px;
+      background: #121212;
+      color: #ffffff;
+      border: none;
+    }
+  }
+`;
